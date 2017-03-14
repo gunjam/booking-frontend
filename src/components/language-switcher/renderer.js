b8a197ad9ff5a233f3d3dbff621815240a221a8e
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2017-03-14 14:06:18 +0000

    Merge phase banner and language switcher

--- src/components/language-switcher/renderer.js
+++ src/components/language-switcher/renderer.js
@@ -6,5 +6,5 @@ exports.renderer = function (input, out) {
   const {originalUrl, language} = out.stream.req;
   const urlWithoutLang = originalUrl.indexOf('lng=') > -1 ? originalUrl.slice(0, -7) : originalUrl;
   const urlWithLangQry = urlWithoutLang.indexOf('?') > -1 ? urlWithoutLang + '&lng=' : '?lng=';
-  template.render({language, urlWithLangQry}, out);
+  template.render(Object.assign({language, urlWithLangQry}, input), out);
 };
